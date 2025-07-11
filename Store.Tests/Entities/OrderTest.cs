0906963afe46b7a645dd01beca2aa63447dd5b67
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-07-11 00:06:29 -0300

    alterações geração do pedido

--- Store.Tests/Entities/OrderTest.cs
+++ Store.Tests/Entities/OrderTest.cs
@@ -1,5 +1,7 @@
 using Store.Domain.Entities;
 using Store.Domain.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace Store.Domain.Entities;
 [TestClass]
 public class OrderTest
@@ -7,16 +9,30 @@ public class OrderTest
     private readonly Customer _customer = new Customer("João da Silva", "joao@gmail.com");
     private readonly Product _product = new Product("Produto 1", 8.5m, true);
     private readonly Discount _discount = new Discount(0.1m, DateTime.Now.AddDays(10));
-    public void Dado_UmPedidoValido_DeveSerVálido()
+    
+    [TestMethod]
+    public void Dado_UmNovoPedido_Valido_NumeroPedidoTem8Caracteres()
     {
         // Arrange é onde você configura o cenário do teste
-        var order = new Order(_customer, 0, null);
+        var order = new Order(_customer, 8, null);
         
 
         // Act é onde você executa a ação que está sendo testada
         var isValid = order.IsValid;
 
         // Assert é onde você verifica se o resultado é o esperado
-        Assert.IsTrue(isValid);
+        Assert.AreEqual(8, order.Number.Length);
+    }
+    [TestMethod]
+    public void Dado_UmPedido_SatusDeveSerAguardandoPagamento()
+    {
+        // Arrange é onde você configura o cenário do teste
+        var order = new Order(_customer, 0, null);
+
+        // Act é onde você executa a ação que está sendo testada
+        var status = order.Status;
+
+        // Assert é onde você verifica se o resultado é o esperado, exemplo, se o status é "Aguardando Pagamento"
+        Assert.AreEqual(EOrderStatus.WaitingPayment, status);
     }
 }
