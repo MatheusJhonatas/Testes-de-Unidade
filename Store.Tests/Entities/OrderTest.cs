a90410d4e06df21ac2002f66ab1e29dc674a8a9b
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-07-10 23:45:40 -0300

    alterações order test

--- Store.Tests/Entities/OrderTest.cs
+++ Store.Tests/Entities/OrderTest.cs
@@ -5,14 +5,13 @@ namespace Store.Domain.Entities;
 public class OrderTest
 {
     private readonly Customer _customer = new Customer("João da Silva", "joao@gmail.com");
-    private readonly Product _product = new Product("Produto 1", "Descrição do Produto 1", 10, true);
-    private readonly Discount _discount = new Discount("Desconto de Teste", 0.1m, DateTime.Now.AddDays(10));
+    private readonly Product _product = new Product("Produto 1", 8.5m, true);
+    private readonly Discount _discount = new Discount(0.1m, DateTime.Now.AddDays(10));
     public void Dado_UmPedidoValido_DeveSerVálido()
     {
         // Arrange é onde você configura o cenário do teste
-        var customer = new Customer("João da Silva", "joao@gmail.com");
-        var order = new Order(customer, 0, null);
-        Assert.AreEqual(8, order.Number.Length);
+        var order = new Order(_customer, 0, null);
+        
 
         // Act é onde você executa a ação que está sendo testada
         var isValid = order.IsValid;
