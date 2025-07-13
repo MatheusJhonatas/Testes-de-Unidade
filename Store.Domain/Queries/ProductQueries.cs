2028e4f70cbeebe32bcde20b049712f1e027381c
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-07-13 13:46:45 -0300

    Alteração isvalid

--- Store.Domain/Queries/ProductQueries.cs
+++ Store.Domain/Queries/ProductQueries.cs
@@ -1,16 +1,16 @@
-using System;
 using System.Linq.Expressions;
 using Store.Domain.Entities;
 
 namespace Store.Domain.Queries;
+
 public static class ProductQueries
 {
     public static Expression<Func<Product, bool>> GetActiveProducts()
     {
-        return product => product.IsActive;
+        return product => product.Active;
     }
     public static Expression<Func<Product, bool>> GetInactiveProducts()
     {
-        return product => !product.IsActive;
+        return product => !product.Active;
     }
 }
